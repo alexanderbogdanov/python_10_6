028261073f8f0e244912172addc14a7d61e52143
alexandrbogdanov <al1029ex@gmail.com>
2024-02-18 13:42:48 +0100

    last task

--- test_basic_python_2.py
+++ test_basic_python_2.py
@@ -27,7 +27,7 @@ def test_dark_theme_by_time_and_user_choice():
     current_time = time(hour=23)
     dark_theme_enabled_by_user = True
 
-    if current_time in range(6, 22) or dark_theme_enabled_by_user == False:
+    if current_time in range(6, 22) or dark_theme_enabled_by_user is False:
         is_dark_theme = False
     else:
         is_dark_theme = True
@@ -72,32 +72,34 @@ def test_find_suitable_user():
 # "Open Browser [Chrome]"
 
 
-# def test_readable_function():
-#     open_browser(browser_name="Chrome")
-#     go_to_companyname_homepage(page_url="https://companyname.com")
-#     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
-
-
 def test_readable_function():
-    print_function(open_browser(browser_name="Chrome"))
+    open_browser(browser_name="Chrome")
     go_to_companyname_homepage(page_url="https://companyname.com")
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = print_function_and_arguments(open_browser, browser_name=browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = print_function_and_arguments(go_to_companyname_homepage, page_url=page_url)
+
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = print_function_and_arguments(find_registration_button_on_login_page,
+                                                 page_url=page_url, button_text=button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
 
 
-def print_function(function, args):
-    print(function.__name__ + args)
+def print_function_and_arguments(func, *args, **kwargs):
+    function_name = func.__name__
+    readable_function_name = ' '.join(word.capitalize() for word in function_name.split('_'))
+    kwargs_values = [str(value) for value in kwargs.values()]
+    kwargs_values_str = ", ".join(kwargs_values)
+    function_info = f"{readable_function_name} [{kwargs_values_str}]"
+    print(function_info)
+    return function_info
